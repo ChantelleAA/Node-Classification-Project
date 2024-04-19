4c8aad48a0d2a13c3b81c2dbf417d7f68a993e83
Chantelle <115734837+ChantelleAA@users.noreply.github.com>
2024-04-19 16:30:49 +0300

    Update run.sh

--- experiments/run.sh
+++ experiments/run.sh
@@ -9,8 +9,5 @@ python gcn.py --dataset='../data/'  --optimizer=Adam --logger=GCN-Cora1-Adam
 
 python gcn.py --dataset='../data/'  --optimizer=Adam --hyperparam=gamma --logger=GCN-Cora1-Adam
 
-python gcn.py --dataset='../data/'  --optimizer=Adam --preconditioner=KFAC --hyperparam=eps --logger=GCN-Cora1-Adam-KFAC
-
-python gcn.py --dataset='../data/'  --optimizer=Adam --preconditioner=KFAC --hyperparam=gamma --logger=GCN-Cora1-Adam-KFAC
 
 
